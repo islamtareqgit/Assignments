feb485bbfb50d855fab3bd6b3b3689a2f970f11c
tareq <tareqabc@gmail.com>
2021-05-07 21:34:50 +0600

    spa updated all files

---
+++ spa updated/ajax_template/create.php
@@ -0,0 +1,14 @@
+<?php
+
+$name = $_POST['name'];
+$email = $_POST['email'];
+$cell = $_POST['cell'];
+$username = $_POST['username'];
+
+
+
+$connection = new mysqli('localhost','root','','ajax 129');
+
+
+$connection -> query("INSERT INTO students (name, email, cell, username) VALUES ('$name','$email','$cell','$username') ");
+
